a009fefed0a707ee2e07b7ce6f7b07217c136829
2003aryansingh <s424.aryansingh@gmail.com>
2023-04-16 16:08:12 +0530

    Created a channel and a publisher

---
+++ send.js
@@ -0,0 +1,44 @@
+const amqp = require('amqplib/callback_api');
+
+
+//code to connect to the  RabbitMQ server
+
+amqp.connect('amqp://localhost', (error, connection) => { });
+
+
+//code to connect and create a channel using amqp
+
+amqp.connect('amqp://localhost', (error, connection) => {
+    if (error) {
+        throw error;
+    }
+    connection.createChannel((error1, channel) => { });
+});
+
+//complete code to publish the message to the created queue
+
+amqp.connect('amqp://localhost', (error0, connection) => {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel((error1, channel) => {
+        if (error1) {
+            throw error1;
+        }
+        let queue = 'hello';
+        let msg = 'Hello world';
+
+        channel.assertQueue(queue, {
+            durable: false
+        });
+
+        channel.sendToQueue(queue, Buffer.from(msg));
+        console.log(" [x] Sent %s", msg);
+    });
+});
+
+
+setTimeout(() => {
+    connection.close();
+    process.exit(0)
+}, 500);
